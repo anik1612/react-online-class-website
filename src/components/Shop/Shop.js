74f589bb636b3e090de3ef56d5e87fda773c0f80
Anik Sarker <aniksarker1612@gmail.com>
2020-08-27 16:41:44 +0800

    some fix for good view in small device

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -23,17 +23,17 @@ const Shop = () => {
         <div className="shop-container border">
             <h1 className="text-center pt-3">Available Course</h1>
             <div className="row content-area container-fluid justify-content-center">
-                <div className="col-md-9 col-7 row course-area border-right border-top pt-3 courses-container">
+                <div className="col-md-9 row course-area border-right border-top pt-3 courses-container">
                     {courses.map(course => <Course course={course} key={course.id} addToCart={addToCart}></Course>)}
                 </div>
 
-                <div className="col-md-3 col-5 cart-area pt-4 cart-container border-top">
+                <div className="col-md-3 cart-area pt-4 cart-container border-top">
                 <h3 className="text-center">Order Summary</h3>
-                    <h4 className="text-center">Cart  {cart.length} </h4>
+                    <h4 className="text-center">Cart: {cart.length} </h4>
                     <ul className="list-group">
                         {cart.map(course => <Cart course={course} />)}
                     </ul>
-                    <button type="button" className="btn btn-success btn-block">
+                    <button type="button" className="btn btn-success btn-block mb-5">
                         Checkout <span className="badge badge-light">$ {totalPrice}</span>
                     </button>
                 </div>
