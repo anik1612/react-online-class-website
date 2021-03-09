aeecdbd7db71964ed3bfba0e5d9141b277b8bd12
Anik <aniksarker1612@gmail.com>
2021-03-09 13:35:53 +0600

    sticky cart added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,29 @@
 import React from 'react';
-import { Navbar, Form, FormControl } from 'react-bootstrap';
+import { Navbar, Form, FormControl, Button } from 'react-bootstrap';
 import './Header.css';
 
 const Header = () => {
-    return (
-            <div className="bg-dark header">
-            <Navbar className="container justify-content-between p-3">
-            <Navbar.Brand href="#home" className="brand-text text-white border border-info rounded p-1">Online School</Navbar.Brand>
-                <Form inline>
-                <FormControl type="search" placeholder="search" className=" mr-sm-2"/>
-                </Form>
-                </Navbar>
-            </div>
-    );
+	return (
+		<Navbar
+			className='container-fluid justify-content-between p-3 bg-dark'
+			sticky='top'
+		>
+			<Navbar.Brand
+				href='#home'
+				className='brand-text text-white border border-info rounded py-2 px-3'
+			>
+				Online School
+			</Navbar.Brand>
+			<Form inline>
+				<FormControl
+					type='search'
+					placeholder='search'
+					className=' mr-sm-2'
+				/>
+				<Button variant='success'>Search</Button>
+			</Form>
+		</Navbar>
+	);
 };
 
 export default Header;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -3,44 +3,63 @@ import { useState } from 'react';
 import Cart from '../Cart/Cart';
 import Course from '../Course/Course';
 import fakeData from '../../fakeData';
-import './Shop.css'
-
+import './Shop.css';
 
 const Shop = () => {
-    const [courses] = useState(fakeData); 
-    const [cart, setCart] = useState([]);
+	const [courses] = useState(fakeData);
+	const [cart, setCart] = useState([]);
+	console.log(fakeData);
+	//add item to cart
+	const addToCart = (course) => {
+		const newCart = [...cart, course];
+		setCart(newCart);
+	};
 
-    //add item to cart
-    const addToCart = (course) => {
-        const newCart = [...cart, course];
-        setCart(newCart);
-    }
+	console.log(cart);
 
-    //get total price
-    const totalPrice = cart.reduce((total, current) => total + current.price, 0);
-    
-    return (
-        <div className="shop-container border">
-            <h1 className="text-center pt-3">Available Course</h1>
-            <div className="row content-area container-fluid justify-content-center">
-                <div className="col-md-9 row course-area border-right border-top pt-3 courses-container">
-                    {courses.map(course => <Course course={course} key={course.id} addToCart={addToCart}></Course>)}
-                </div>
+	//get total price
+	const totalPrice = cart.reduce(
+		(total, current) => total + current.price,
+		0
+	);
 
-                <div className="col-md-3 cart-area pt-4 cart-container border-top">
-                <h3 className="text-center">Order Summary</h3>
-                    <h4 className="text-center">Cart: {cart.length} </h4>
-                    <ul className="list-group">
-                        {cart.map(course => <Cart course={course} />)}
-                    </ul>
-                    <button type="button" className="btn btn-success btn-block mb-5">
-                        Checkout <span className="badge badge-light">$ {totalPrice}</span>
-                    </button>
-                </div>
+	return (
+		<div className='shop-container border'>
+			<h1 className='text-center pt-3'>Available Course</h1>
+			<div className='row content-area container-fluid justify-content-center'>
+				<div className='col-md-9 row course-area border-right border-top pt-3 courses-container'>
+					{courses.map((course) => (
+						<Course
+							course={course}
+							key={course.id}
+							addToCart={addToCart}
+						></Course>
+					))}
+				</div>
 
-            </div>
-        </div>
-    );
+				<div className='col-md-3'>
+					<div className='cart-area pt-4 cart-container border-top sticky-top'>
+						<h3 className='text-center'>Order Summary</h3>
+						<h4 className='text-center'>Cart: {cart.length} </h4>
+						<ul className='list-group'>
+							{cart.map((course) => (
+								<Cart course={course} />
+							))}
+						</ul>
+						<button
+							type='button'
+							className='btn btn-success btn-block mb-5'
+						>
+							Checkout 
+							<span className='badge badge-light ml-1'>
+								$ {totalPrice}
+							</span>
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Shop;
